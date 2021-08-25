9b950acff043244dd2d32edda0ef17b436efed31
Vinicius Paes Berna <vini_berna2.0@hotmail.com>
2021-08-25 14:55:14 -0300

    fix: CharacterDetails animation

--- src/components/layout/room/Character/CharacterDetails.tsx
+++ src/components/layout/room/Character/CharacterDetails.tsx
@@ -1,4 +1,5 @@
 import { Flex, IconButton, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { BsFileText } from "react-icons/bs";
 import { Input } from "../../../common";
@@ -7,40 +8,62 @@ interface CharacterDatailsProps{
   openCharacterSheet: () => void
 }
 
+const loaderVariants = {
+  animationOne: {
+    opacity: [0, 1],
+    transition: {
+      opacity: {
+        duration: 1,
+        ease: "easeInOut",
+      },
+    }
+  }
+}
+
 export function CharacterDetails({openCharacterSheet}: CharacterDatailsProps){
   const [hp, setHp] = useState('')
 
   return (
-    <Flex
-      align="center"
-      justify="space-between"
-      w="100%"
-      mr="4"
+    <motion.div
+      style={{
+        width: '100%',
+        marginRight: '1rem',
+        opacity: '0'
+      }}
+      variants={loaderVariants}
+      animate="animationOne"
     >
-      <Flex ml="4" flex="1" flexDir="column" >
-        <Text fontSize="xl" ml="2">HaloSara121</Text>
-        <Input
-          name="hp"
-          type="number"
-          placeholder="hp"
-          _placeholder={{color:"gray.600"}}
-          w="10" 
-          value={hp}
-          onChange={e => setHp(e.target.value)}
+      <Flex
+        align="center"
+        justify="space-between"
+        w="100%"
+        mr="4"
+      >
+        <Flex ml="4" flex="1" flexDir="column" >
+          <Text fontSize="xl" ml="2">HaloSara121</Text>
+          <Input
+            name="hp"
+            type="number"
+            placeholder="hp"
+            _placeholder={{color:"gray.600"}}
+            w="10" 
+            value={hp}
+            onChange={e => setHp(e.target.value)}
+          />
+        </Flex>
+
+        <IconButton
+          onClick={openCharacterSheet}
+          colorScheme="yellow"
+          aria-label="Ficha do personagem"
+          icon={<BsFileText size="24"/>}
+          _focus={{
+            outline: "none"
+          }}
+          size="md"
+          ml="auto"
         />
       </Flex>
-
-      <IconButton
-        onClick={openCharacterSheet}
-        colorScheme="yellow"
-        aria-label="Ficha do personagem"
-        icon={<BsFileText size="24"/>}
-        _focus={{
-          outline: "none"
-        }}
-        size="md"
-        ml="auto"
-      />
-    </Flex>
+    </motion.div>
   )
 }
